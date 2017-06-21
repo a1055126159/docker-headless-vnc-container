fe37314d1dab55b16dc33c0188f8ec6ded7fa0a4
a1055126159 <1055126159@qq.com>
2017-06-21 17:11:47 +0800

    Create flash.sh

---
+++ src/centos/install/flash.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+### every exit != 0 fails the script
+
+curl -o flash.rpm https://fpdownload.adobe.com/pub/flashplayer/pdc/26.0.0.131/flash-player-ppapi-26.0.0.131-release.x86_64.rpm
+rpm -ivh flash-player-ppapi-26.0.0.131-release.x86_64.rpm
+rm flash-player-ppapi-26.0.0.131-release.x86_64.rpm
