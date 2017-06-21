533fd07d49a4d88cb667e22c15b890df5d55e03e
a1055126159 <1055126159@qq.com>
2017-06-21 17:37:04 +0800

    Update flash.sh

--- src/centos/install/flash.sh
+++ src/centos/install/flash.sh
@@ -2,5 +2,5 @@
 ### every exit != 0 fails the script
 
 curl -o flash.rpm https://fpdownload.adobe.com/pub/flashplayer/pdc/26.0.0.131/flash-player-ppapi-26.0.0.131-release.x86_64.rpm
-rpm -ivh flash-player-ppapi-26.0.0.131-release.x86_64.rpm
-rm flash-player-ppapi-26.0.0.131-release.x86_64.rpm
+rpm -ivh flash.rpm
+rm flash.rpm
